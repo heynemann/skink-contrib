584a2f08dceaa5aef3e072240f1b69dacb64f363
Bernardo Heynemann <heynemann@gmail.com>
2009-04-21 23:55:56 -0300

    Added configurations file

---
+++ skink-alert/skink-alert/context.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import sys
+from ConfigParser import ConfigParser
+from os.path import dirname, abspath, join
+root_path = abspath(join(dirname(__file__), "../"))
+sys.path.insert(0, root_path)
+
+class AlertContext:
+    instance = None
+    @classmethod
+    def current(cls):
+        if not cls.instance:
+            cls.instance = cls()
+            config = ConfigParser()
+            config.read(join(root_path, "config.ini"))
+
+            #General
+            cls.instance.debug_mode = config.get("General", "debug_mode").lower() == "true"
+            cls.instance.full_screen = config.get("General", "full_screen")
+            cls.instance.width = int(config.get("General", "width"))
+            cls.instance.height = int(config.get("General", "height"))
+            cls.exit_key = config.get("General","exit_key")
+
+            #Skink Server
+            cls.instance.skink_address = config.get("SkinkServer","skink_address")
+
+            #Layout
+            cls.instance.font_size = int(config.get("Layout","font_size"))
+
+            unknown_bg_red = int(config.get("Layout","unknown_bg_red"))
+            unknown_bg_green = int(config.get("Layout","unknown_bg_green"))
+            unknown_bg_blue = int(config.get("Layout","unknown_bg_blue"))
+            unknown_font_red = int(config.get("Layout","unknown_font_red"))
+            unknown_font_green = int(config.get("Layout","unknown_font_green"))
+            unknown_font_blue = int(config.get("Layout","unknown_font_blue"))
+            cls.instance.unknown_bg = (unknown_bg_red, unknown_bg_green, unknown_bg_blue)
+            cls.instance.unknown_font = (unknown_font_red, unknown_font_green, unknown_font_blue)
+
+            broken_bg_red = int(config.get("Layout","broken_bg_red"))
+            broken_bg_green = int(config.get("Layout","broken_bg_green"))
+            broken_bg_blue = int(config.get("Layout","broken_bg_blue"))
+            broken_font_red = int(config.get("Layout","broken_font_red"))
+            broken_font_green = int(config.get("Layout","broken_font_green"))
+            broken_font_blue = int(config.get("Layout","broken_font_blue"))
+            cls.instance.broken_bg = (broken_bg_red, broken_bg_green, broken_bg_blue)
+            cls.instance.broken_font = (broken_font_red, broken_font_green, broken_font_blue)
+
+            success_bg_red = int(config.get("Layout","success_bg_red"))
+            success_bg_green = int(config.get("Layout","success_bg_green"))
+            success_bg_blue = int(config.get("Layout","success_bg_blue"))
+            success_font_red = int(config.get("Layout","success_font_red"))
+            success_font_green = int(config.get("Layout","success_font_green"))
+            success_font_blue = int(config.get("Layout","success_font_blue"))
+            cls.instance.success_bg = (success_bg_red, success_bg_green, success_bg_blue)
+            cls.instance.success_font = (success_font_red, success_font_green, success_font_blue)
+
+            #Audio
+            cls.instance.audio_root = config.get("Audio","audio_root")
+            cls.instance.audio_broken = config.get("Audio","audio_broken")
+            
+        return cls.instance
+    
+    @classmethod
+    def path_for_audio_file(cls, audio_file):
+        ctx = cls.current()
+        if ctx.audio_root.startswith("/"):
+            return join(ctx.audio_root, audio_file)
+        else:
+            return join(root_path, ctx.audio_root, audio_file)
--- skink-alert/skink-alert/game.py
+++ skink-alert/skink-alert/game.py
@@ -27,57 +27,45 @@ import simplejson
 import pygame
 from pygame.locals import *
 from text_rect import *
+from context import AlertContext
 
-RESOLUTION_VERTICAL=0
-RESOLUTION_HORIZONTAL=0
-FULL_SCREEN_MODE = True
-DEBUG_MODE = False
-
-RED_BG = (227,52,52)
-GREEN_BG = (11,143,1)
-YELLOW_BG = (255, 248, 135)
-
-BLACK_TEXT = (0,0,0)
-WHITE_TEXT = (255,255,255)
-
-TEXT_SIZE = 24
 LOGO_HEIGHT = 136
 MESSAGE_OFFSET_VERT = 60
-
-FAILURE_SOUND = "doh.wav"
 
 class Game(object):
 
     def __init__(self):
         self.initialize()
+        self.ctx = AlertContext.current()
         self.resources = Resources()
         self.resources.preload()
         self.is_failing = False
 
     def log(self, message):
-        if DEBUG_MODE:
+        if self.ctx.debug_mode:
             print message
 
     def initialize(self):
         pygame.init()
 
     def show(self):
-        if DEBUG_MODE:
+        if self.ctx.debug_mode:
             vert_res=400
             horiz_res=800
             full_screen = False
         else:
-            vert_res = RESOLUTION_VERTICAL
-            horiz_res = RESOLUTION_HORIZONTAL
-            full_screen = FULL_SCREEN_MODE
+            vert_res = self.ctx.height
+            horiz_res = self.ctx.width
+            full_screen = self.ctx.full_screen
 
         if full_screen:
             self.window = pygame.display.set_mode((horiz_res, vert_res), pygame.FULLSCREEN)
         else:
             self.window = pygame.display.set_mode((horiz_res, vert_res))
+            
         pygame.display.set_caption('Skink Alert')
         self.screen = pygame.display.get_surface()
-        self.display_message("Retrieving...", YELLOW_BG, BLACK_TEXT)
+        self.display_message("Retrieving...", self.ctx.unknown_bg, self.ctx.unknown_font)
 
     def loop(self):
         event_handler = EventHandler(self)
@@ -107,15 +95,15 @@ class Game(object):
                     errors.append("FAILURE #%d\n    Project:%s\n    Committer:%s\nDate: %s\n\n" % (error_number, project["name"], build["commitCommitter"], build["commitCommitterDate"]))
 
         if not errors:
-            self.display_message("ALL BUILDS GREEN")
+            self.display_message("ALL BUILDS GREEN", self.ctx.success_bg, self.ctx.success_font)
             self.is_failing = False
         else:
-            self.display_message("BUILD FAILING:\n%s" % "\n".join(errors), RED_BG)
+            self.display_message("BUILD FAILING:\n%s" % "\n".join(errors), self.ctx.broken_bg, self.ctx.broken_font)
             if not self.is_failing:
                 self.resources.play_broken_build_sound()
                 self.is_failing = True
 
-    def display_message(self, message, rgb = GREEN_BG, text_color=WHITE_TEXT):
+    def display_message(self, message, rgb, text_color):
         self.resources.print_bg(rgb, self.screen)
         self.resources.print_message(message, text_color, rgb, self.screen)
         self.resources.print_logo_image(self.screen)
@@ -125,6 +113,7 @@ class Game(object):
 class EventHandler(object):
     def __init__(self, game):
         self.game = game
+        self.ctx = AlertContext.current()
 
     def process_events(self, events):
         #no events raised.
@@ -133,24 +122,23 @@ class EventHandler(object):
         for event in events:
             if self.check_for_quit(event):
                 return "QUIT"
-            if DEBUG_MODE and event.type == KEYDOWN:
+            if self.ctx.debug_mode and event.type == KEYDOWN:
                 if event.unicode == 's':
                     self.resources.play_broken_build_sound()
-                if event.unicode == 'r':
-                    self.game.display_message("FAIL", RED_BG)
-                if event.unicode == 'g':
-                    self.game.display_message("SUCCESS", GREEN_BG)
             return event
 
     def check_for_quit(self, event):
         if event.type == QUIT:
             return True
         elif event.type == KEYDOWN:
-            if event.unicode == 'q':
+            if event.unicode == self.ctx.exit_key:
                 return True
         return False
 
 class Resources(object):
+    def __init__(self):
+        self.ctx = AlertContext.current()
+
     def preload(self):
         self.load_broken_build_sound()
         self.load_logo_image()
@@ -165,7 +153,8 @@ class Resources(object):
 
         if not pygame.mixer:
             return NoneSound()
-        fullname = join(root_path, "skink-alert", FAILURE_SOUND)
+
+        fullname = self.ctx.path_for_audio_file(self.ctx.audio_broken)
         try:
             sound = pygame.mixer.Sound(fullname)
         except pygame.error, message:
@@ -192,7 +181,7 @@ class Resources(object):
 
     def print_message(self, message, text_color, background_color, screen):
         if pygame.font:
-            font = pygame.font.Font(None, TEXT_SIZE)
+            font = pygame.font.Font(None, self.ctx.font_size)
             display_info = pygame.display.Info()
             screen_width = display_info.current_w
             screen_height = display_info.current_h
@@ -208,12 +197,12 @@ class Resources(object):
         screen.blit(background, (0,0))
 
     def print_exit_message(self, text_color, background_color, screen):
-        font = pygame.font.Font(None, TEXT_SIZE)
+        font = pygame.font.Font(None, self.ctx.font_size)
         display_info = pygame.display.Info()
         screen_width = display_info.current_w
         screen_height = display_info.current_h
         rect = pygame.Rect((0, screen_height - 30, screen_width, 30))
-        rendered_text = render_textrect("Press q to QUIT", font, rect, text_color, background_color, 1)
+        rendered_text = render_textrect("Press %s to QUIT" % self.ctx.exit_key, font, rect, text_color, background_color, 1)
         if rendered_text:
             screen.blit(rendered_text, rect.topleft)
 
@@ -222,6 +211,7 @@ class Monitor(object):
 
     @classmethod
     def get_last_status(cls, callback):
+        ctx = AlertContext.current()
         if cls.is_retrieving: return
         cls.is_retrieving = False
         cls.callback = callback
@@ -230,10 +220,11 @@ class Monitor(object):
 
     @classmethod
     def connect_and_retrieve_status(cls):
+        ctx = AlertContext.current()
         try:
             cls.is_retrieving = True
             time.sleep(3)
-            conn = httplib.HTTPConnection("localhost:8082")
+            conn = httplib.HTTPConnection(ctx.skink_address)
             url = "/status?_=%d" % (random.random() * 1000)
             conn.request("GET", url)
             r1 = conn.getresponse()
